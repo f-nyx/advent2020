7ff1ff54930c32922ae1f28bcf73f7d7c87503ab
f-nyx <dnyx@riseup.net>
2020-12-08 08:57:58 -0300

    Day 8: auto-fix program

--- 08-game-boot-fix/boot_vm.js
+++ 08-game-boot-fix/boot_vm.js
@@ -1,57 +1,72 @@
 const fs = require('fs');
 
 const parseOp = (line) => ({
-    op: line.substr(0, line.indexOf(' ')),
+    name: line.substr(0, line.indexOf(' ')),
     arg: Number(line.substr(line.indexOf(' ') + 1))
 });
-
+const [ ACC, NOP, JMP ] = ['acc', 'nop', 'jmp'];
 const Operations = {
-    acc(context, arg) {
+    [ACC]: (context, arg) => {
         context.r1 += arg;
         context.ip += 1;
     },
-    nop(context) {
+    [NOP]: (context) => {
         context.ip += 1;
     },
-    jmp(context, arg) {
+    [JMP]: (context, arg) => {
         context.ip += arg;
     }
 };
 
-function printStackTrace(ops, trace) {
-    const stacktrace = trace.reverse().map(ip => `Ln ${ip}: ${ops[ip].op} ${ops[ip].arg}`);
-    console.log('Infinite loop detected. Stacktrace (last 15 frames):', JSON.stringify(stacktrace.slice(0, 15), null, 2));
-}
-
-function exec(lines) {
-    const context = { ip: 0, r1: 0 };
-    const ops = lines.map(parseOp);
-    const trace = [];
+function exec(ops) {
+    const context = { ip: 0, r1: 0, ret: null, trace: [] };
 
     while (context.ip < ops.length) {
-        const { op, arg } = ops[context.ip];
+        const { name, arg } = ops[context.ip];
 
-        if (trace.includes(context.ip)) {
-            printStackTrace(ops, trace);
-            return context.r1;
+        if (context.trace.includes(context.ip)) {
+            return context;
         } else {
-            trace.push(context.ip);
+            context.trace.push(context.ip);
         }
 
-        Operations[op](context, arg);
+        Operations[name](context, arg);
     }
 
-    return context.r1;
+    return { ...context, ret: context.r1 };
 }
 
-function run(programFile) {
-    const lines = fs.readFileSync(programFile)
+function autofix(ctx, ops) {
+    const Fixups = {
+        [NOP]: (op) => op.name = JMP,
+        [JMP]: (op) => op.name = NOP
+    };
+    let op;
+    do {
+        op = ops[ctx.trace.pop()];
+    } while(!Fixups[op.name]);
+
+    Fixups[op.name](op);
+
+    const newCtx = exec(ops);
+
+    if (newCtx.ret) {
+        return newCtx;
+    } else {
+        return autofix(ctx, ops);
+    }
+}
+
+function run() {
+    const ops = fs.readFileSync('input.txt')
         .toString()
         .split('\n')
-        .filter(Boolean);
-    const acc = exec(lines);
-    console.log(`${programFile}: ${acc}`);
+        .filter(Boolean)
+        .map(parseOp);
+    const ctx = exec(ops);
+    console.log('infinite loop detected, current value:', ctx.r1);
+    const newCtx = autofix(ctx, ops);
+    console.log('program is not fixed', newCtx.ret);
 }
 
-run('input_error.txt');
-run('input_ok.txt');
+run();
